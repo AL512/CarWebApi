4e48de92ee3f3fb5f7b3ba1e92943c9de188e7a2
Александр Белокрылов <AL512@mail.ru>
2025-06-29 21:21:42 +0300

    Added WithPrice in CarFilterExtensions

---
+++ CarWebApi/Extensions/CarFilterExtensions.cs
@@ -0,0 +1,17 @@
+﻿using CarWebApi.PageParams;
+
+namespace CarWebApi.Extensions;
+
+public static class CarFilterExtensions
+{
+    public static CarFilterParams WithPrice(
+        this CarFilterParams spec,
+        decimal? minPrice,
+        decimal? maxPrice)
+    {
+        return spec.WithCustomFilter(car => 
+            (!minPrice.HasValue || car.Price >= minPrice.Value) &&
+            (!maxPrice.HasValue || car.Price <= maxPrice.Value));
+    }
+
+}
